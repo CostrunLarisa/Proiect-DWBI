22c33dc7e7517b01166b309bf7053e64111252b0
Costrun Larisa-Bianca <me_bibi7@yahoo.com>
2024-01-27 18:57:01 +0200

    added enums

---
+++ BE/src/main/java/com/unibuc/ro/utils/StatusLivrare.java
@@ -0,0 +1,8 @@
+package com.unibuc.ro.utils;
+
+public enum StatusLivrare {
+    FINALIZATA,
+    IN_CURS_DE_LIVRARE,
+    IN_CURS_DE_PROCESARE,
+    INITIATA
+}
---
+++ BE/src/main/java/com/unibuc/ro/utils/TipDePlata.java
@@ -0,0 +1,8 @@
+package com.unibuc.ro.utils;
+
+public enum TipDePlata {
+    CASH,
+    MASTERCARD,
+    TRANSFER_BANCAR,
+    REVOLUT
+}
