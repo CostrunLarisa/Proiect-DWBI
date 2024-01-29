6f47bea1d136d29dcba595ec3a991a0f9fcbfe4c
Costrun Larisa-Bianca <me_bibi7@yahoo.com>
2024-01-29 22:23:38 +0200

    added shops page

--- UI/src/app/shop/shop.component.ts
+++ UI/src/app/shop/shop.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../authservice";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-shop',
@@ -8,13 +9,35 @@ import {Router} from "@angular/router";
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
-
-
-  constructor(private authService: AuthService, private router : Router) { }
+  shops: any[] = [];
+  constructor(private authService: AuthService,
+              private route: ActivatedRoute,
+              private router : Router,
+              private http: HttpClient,) { }
 
   ngOnInit(): void {
     if (!this.authService.isLoggedIn()) {
       this.router.navigate(['/error']);
     }
+    this.route.params.subscribe(params => {
+      this.getShops();
+    });
+  }
+
+  private getShops() {
+    const headers = new HttpHeaders({
+      // @ts-ignore
+      'Authorization-Token': this.authService.getAuthToken(),
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:8080'
+    });
+    this.http.get<any[]>('http://localhost:8080/shops', {headers }).subscribe(
+      (response: any) => {
+        this.shops = response;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
   }
 }
