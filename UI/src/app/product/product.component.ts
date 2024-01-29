a5833b4c3a96afc615356ca98e6797d0651d8e12
Costrun Larisa-Bianca <me_bibi7@yahoo.com>
2024-01-29 21:26:32 +0200

    added authservice logic

--- UI/src/app/product/product.component.ts
+++ UI/src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../authservice";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-product',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private authService: AuthService, private router : Router) { }
 
   ngOnInit(): void {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/error']);
+    }
   }
-
 }
--- UI/src/app/shop/shop.component.ts
+++ UI/src/app/shop/shop.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../authservice";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-shop',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private authService: AuthService, private router : Router) { }
 
   ngOnInit(): void {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/error']);
+    }
   }
-
 }
