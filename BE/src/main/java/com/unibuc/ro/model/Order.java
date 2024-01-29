2dfcfb8c34ebef97705ffd4260cfdfa0bafa59bf
StefaniaCarutasu <stefania_carutasu@yahoo.com>
2024-01-29 15:38:38 +0200

    order model

--- BE/src/main/java/com/unibuc/ro/model/Order.java
+++ BE/src/main/java/com/unibuc/ro/model/Order.java
@@ -20,7 +20,7 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
-    private Long id;
+    private Long orderId;
 
     @NotNull
     private Date datePlaced;
