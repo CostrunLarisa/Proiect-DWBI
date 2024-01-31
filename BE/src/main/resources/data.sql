f7beabf87971127826ab9f71db10f9ade2d66bbf
anabiancachiricu <62220976+anabiancachiricu@users.noreply.github.com>
2024-02-01 00:08:33 +0200

    fixes and init script

---
+++ BE/src/main/resources/data.sql
@@ -0,0 +1,25 @@
+INSERT INTO shop(shop_id, name, categorie) VALUES (1, 'Auchan', 2);
+INSERT INTO shop(shop_id, name, categorie) VALUES (2, 'Mega Image', 1);
+INSERT INTO shop(shop_id, name, categorie) VALUES (3, 'Delice', 5);
+INSERT INTO shop(shop_id, name, categorie) VALUES (4, 'LeBonBon', 5);
+INSERT INTO shop(shop_id, name, categorie) VALUES (5, 'Ana State', 5);
+INSERT INTO shop(shop_id, name, categorie) VALUES (6, 'Profi', 1);
+INSERT INTO shop(shop_id, name, categorie) VALUES (7, 'Cora', 1);
+INSERT INTO shop(shop_id, name, categorie) VALUES (8, 'Carrefour', 2);
+INSERT INTO shop(shop_id, name, categorie) VALUES (9, 'Artizante', 4);
+INSERT INTO shop(shop_id, name, categorie) VALUES (10, 'Bobocica', 4);
+INSERT INTO shop(shop_id, name, categorie) VALUES (11, 'La baba', 3);
+INSERT INTO shop(shop_id, name, categorie) VALUES (12, 'Afi', 6);
+INSERT INTO shop(shop_id,name, categorie) VALUES (13, 'ParkLake', 6);
+
+INSERT INTO product(description, discount, in_stock, name, price, stock, shop_id) VALUES
+('Iaurt grscesc cremos', '0.5', '1', 'Iaurt', 3, 20, 2);
+INSERT INTO product(description, discount, in_stock, name, price, stock, shop_id) VALUES
+    ('Prajitura insiropata cu cacao si rom', '0', '1', 'Amandina', '8', 16, 3);
+INSERT INTO product(description, discount, in_stock, name, price, stock, shop_id) VALUES
+    ('Morcovi proaspeti', '0', '1', 'Morcovi', 2, 80, 10);
+INSERT INTO product(description, discount, in_stock, name, price, stock, shop_id) VALUES
+    ('Pizza clasicaa', '10', '1', 'Pizza clasic', 20, 10, 13);
+
+
+
